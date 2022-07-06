a4cff9c7238f3f7abab971241025aae548b1e8e8
hanmeng <hanm@buaa.edu.cn>
2022-07-07 00:00:24 +0800

    update c++ 11 to 14 and fix a function param bug

--- kdtree.cuh
+++ kdtree.cuh
@@ -12,7 +12,7 @@ void    buildKDTree(int * bucketIndex, int * bucketLength, float3 * ptr, int kd_
 
 __global__ void devide(float3* dPoints, int * bucketIndex, int * bucketLength, int numPartition, int bufferLength);
 
-__global__ void generateBoundbox(int * bucketIndex, int * bucketLength, float3 * dPoints, int numPartition, float3 * up, float3 * down);
+__global__ void generateBoundbox(int * bucketIndex, int * bucketLength, float3 * dPoints, int numPartition, int bufferLength, float3 * up, float3 * down);
 
 void sample(int * bucketIndex,int * bucketLength,float3 * ptr, int bucketSize, float3 * up, float3 * down, float4 * bucketTable, int sample_number, float3* result);
 
